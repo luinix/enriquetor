c8692704cb797355f393c15d0093b4da4474acff
Luis Rivas Vañó <luis.rivas@fundingcircle.com>
2017-09-12 12:11:23 +0100

    One more chuffich

--- virtual_enrique/lexicon/suffixes.rb
+++ virtual_enrique/lexicon/suffixes.rb
@@ -6,6 +6,7 @@ module VirtualEnrique
           "I guarantee you",
           "yes or yes",
           "I don't give a chit about it",
+          "it's horrible",
           "we don't really have a choice do we?",
           "and well you know the rest" ].sample
       end
