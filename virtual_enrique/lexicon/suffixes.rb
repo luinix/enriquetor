c8ea45f52a07358befa860e62cbe8290c108e81a
Luis Rivas Vañó <luis.rivas@fundingcircle.com>
2017-09-18 16:21:47 +0100

    Adds a new suffix, as simple as that

--- virtual_enrique/lexicon/suffixes.rb
+++ virtual_enrique/lexicon/suffixes.rb
@@ -8,6 +8,7 @@ module VirtualEnrique
           "I don't give a chit about it",
           "it's horrible",
           "we don't really have a choice do we?",
+          "as simple as that",
           "and well you know the rest" ].sample
       end
     end
